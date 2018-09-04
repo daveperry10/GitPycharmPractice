7f8ae2aa846b0a78a0a919094eece22261bdd337
Dave Perry <daveperry10@gmail.com>
2018-09-03 20:53:51 -0700

    load from csv into table, retrieve data and make a plot

--- Analytics.py
+++ Analytics.py
@@ -1,5 +1,29 @@
-def doit():
-    print("hello world 2")
 
+import pymysql as db
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
+#### GET CONNECTION ####
+connection = db.connect(user='analytics_user', password='sumdata', db='analytics', autocommit=True)
+cursor = connection.cursor()
 
+#### GET DATA ####
+query = 'select * from historical_data'
+df = pd.read_sql('select * from historical_data', connection)
+
+fig, ax1 = plt.subplots()
+
+ax1.set_xlabel('Year')
+ax1.set_ylabel('Home Price')
+ax1.plot(df['yr'], df['real_home_price'], color='red')
+ax1.plot(df['yr'], df['nominal_home_price'], color='orange')
+ax1.tick_params(axis='y')
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+ax2.set_ylabel('CPI')  # we already handled the x-label with ax1
+ax2.plot(df['yr'], df['CPI'], color='blue')
+ax2.tick_params(axis='y')
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+plt.show()
--- Utilities.py
+++ Utilities.py
@@ -3,17 +3,36 @@ import pymysql as db
 import pandas as pd
 from pathlib import Path
 
-#data_folder = Path("source_data/text_files/")
-#file_to_open = data_folder / "raw_data.txt"
-##f = open(file_to_open)
-#print(f.read())
+#### Get CONNECTION ####
+connection = db.connect(user='analytics_user', password='sumdata', db='analytics', autocommit=True)
+cursor = connection.cursor()
 
-print('hello world')
+###############################################################################################
+#    BULK LOAD                                                                                #
+###############################################################################################
 
-connection = db.connect(user='analytics_user', password='sumdata', db='world')
-query = 'select * from city where population > 5000000 order by population desc limit 20;'
-df = pd.read_sql(query, connection)
-print (df)
+def bulkLoad():
+    #### READ CSV ####
+    data_folder = Path("C:/Users/Dave/Documents/Sum/Data")
+    file_to_open = data_folder / "HP Data 1890.csv"
+    df1 = pd.read_csv(file_to_open, index_col=0)
+    # print(df1)
 
-for i in [['a','b'],['c','d']]:
-    c.execute("insert into T (F1,F2) values (%s, %s)", (i[0], i[1]))
+    #### INSERT ####
+
+    for row in df1.itertuples():
+        args = (row[0], row[1], row[2], row[3])
+        query = "insert into historical_data (yr, real_home_price, nominal_home_price, CPI) values (%s,%s,%s,%s)"
+        try:
+            cursor.execute(query, args)
+        except:
+            print("Data already exists for date "+ str(row[0]))
+            pass
+    connection.commit()
+    return
+
+
+
+
+
+
